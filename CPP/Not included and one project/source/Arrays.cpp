31aec05ff6c79a34a3ee68d16fb3bad2988d8ddd
EnergoStalin <alex.vipers@mail.ru>
2020-01-22 10:23:48 +0300

    Specify array lib

--- CPP/Not included and one project/source/Arrays.cpp
+++ CPP/Not included and one project/source/Arrays.cpp
@@ -1,6 +1,5 @@
 #pragma once
-#include <iostream>
-#pragma once
+#include <iostream>
 #include <cstdlib>
 
 using std::cout;
@@ -9,33 +8,33 @@ using std::endl;
 namespace ArrayTools {
 	enum OutputFormat //For function ShowArray()
 	{
-		DEFAULT=0,
-		LINE,
-		TAB_LINE,
-		LINE_NO_SPACE
+		ARRAY_DEFAULT=0,
+		ARRAY_LINE,
+		ARRAY_TAB_LINE,
+		ARRAY_LINE_NO_SPACE
 	};
 
-	void FillIntArray(int arr[], const int size) {
+	void fill_int_array(int arr[], const int size) {
 		for(int i = 0; i < size; i++) {
 			arr[i] = rand() % 10;
 		}
 	}
 
 	template<typename T>
-	void ShowArray(const T arr[], const int size, const int format = DEFAULT) {
+	void show_array(const T arr[], const int size, const int format = DEFAULT) {
 		if(format == DEFAULT) {
 			for(int i = 0; i < size; i++) {
 				cout << "I - " << (i + 1) << "\tValue: " << arr[i] << "\tAt: " << (arr+i) << endl;
 			}
-		} else if(format == LINE){
+		} else if(format == ARRAY_LINE){
 			for(int i = 0; i < size; i++) {
 				cout << arr[i] << " ";
 			} cout << endl;
-		} else if(format == TAB_LINE) {
+		} else if(format == ARRAY_TAB_LINE) {
 			for(int i = 0; i < size; i++) {
 				cout << arr[i] << '\t';
 			} cout << endl;
-		} else if(format == LINE_NO_SPACE) {
+		} else if(format == ARRAY_LINE_NO_SPACE) {
 			for(int i = 0; i < size; i++) {
 				cout << arr[i];
 			} cout << endl;
@@ -43,28 +42,28 @@ namespace ArrayTools {
 	}
 	
 	template<typename T>
-	void ArrayCopy(T *arr, T *to, int size) {
+	void array_copy(T *arr, T *to, int size) {
 		for(int i = 0; i < size; i++) {
 			to[i] = arr[i];
 		}
 	}
 	
 	template<typename T>
-	bool ArrayEquals(T *arr,T *to, int size) {
+	bool array_equals(T *arr,T *to, int size) {
 		for(int i = 0; i < size; i++) {
 			if(arr[i] != to[i]) { return false; }
 		} return true;
 	}
 	
 	template<typename T>
-	void ArrayAssign(T *&arr, T *&to, const int size) {
+	void array_assign(T *&arr, T *&to, const int size) {
 		for(int i = 0; i < size; i++) {
 			arr[i] = to[i];
 		}
 	}
 	
 	template<typename T>
-	void push_back(T *&arr, int &size, const T value) {
+	void push_back_array(T *&arr, int &size, const T value) {
 		T *newArr = new T[size + 1];
 		for(int i = 0; i < size; i++) {
 			newArr[i] = arr[i];
@@ -77,7 +76,7 @@ namespace ArrayTools {
 	}
 	
 	template<typename T>
-	void pop_back(T *&arr, int &size) {
+	void pop_back_array(T *&arr, int &size) {
 		T *newArr = new T[--size];
 		for(int i = 0; i < size; i++) {
 			newArr[i] = arr[i];
@@ -88,7 +87,7 @@ namespace ArrayTools {
 	}
 	
 	template<typename T>
-	T* Link_Array(const T arr1[], const T arr2[], const int size1, const int size2) {
+	T* link_Array(const T arr1[], const T arr2[], const int size1, const int size2) {
 		T *arr3 = new T[size1 + size2];
 		for(int i = 0; i < size1; i++) {
 			arr3[i] = arr1[i];
@@ -99,7 +98,7 @@ namespace ArrayTools {
 		return arr3;
 	}
 	template<typename T>
-	T FlipArray(T &arr, int length) {
+	T flip_array(T &arr, int length) {
 		T array[length];
 		for(int i = length, k = 0; i >= 0, k <= length; i--,k++) {
 			array[k] = arr[i];
